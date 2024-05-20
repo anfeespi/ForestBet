10b1960f327cd4c9275f125e2f3a1cb06cf8a130
andre <andre@LaMaquinola>
2024-05-20 09:39:09 -0500

    VERSION 0.4 NOW THE WIKI IT'S ONLINE

--- master/src/main/java/co/edu/unbosque/forestbet/master/controller/WikiController.java
+++ master/src/main/java/co/edu/unbosque/forestbet/master/controller/WikiController.java
@@ -9,71 +9,142 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
+import co.edu.unbosque.forestbet.master.model.Circuit;
 import co.edu.unbosque.forestbet.master.model.Pilot;
+import co.edu.unbosque.forestbet.master.model.Race;
 import co.edu.unbosque.forestbet.master.model.Scuderia;
 import co.edu.unbosque.forestbet.master.util.HttpConnectionsAndRequests;
 
 @RestController
 @RequestMapping
 public class WikiController {
-	
-	@GetMapping("/showScuderias")
-	public String showScuderias(Model model) {
+
+	public Integer chargeScuderias(Model model) {
 		StringBuilder request = new StringBuilder("http://localhost:8082/scuderias/chargeScuderias");
 		String method = "GET";
-		
+
 		String response = HttpConnectionsAndRequests.doARequest(request.toString(), method);
-		
-		if(response.equals("Cargadas exitosamente")) {
-			StringBuilder requestPartTwo = new StringBuilder("http://localhost:8082/scuderias/showScuderias");
-			
-			String responsePartTwo = HttpConnectionsAndRequests.doARequest(requestPartTwo.toString(), method);
-			
-			Gson gson = new Gson();
-			
-			@SuppressWarnings("unchecked")
-			ArrayList<Scuderia> scuderias = gson.fromJson(responsePartTwo, ArrayList.class);
-			System.out.println(scuderias.toString());
-			model.addAttribute("scuderias", scuderias);
-			
-			return "202";			
-		}else {
-			return "500";
+
+		if (response.equals("Cargadas exitosamente")) {
+			return 202;
 		}
+
+		return 500;
 	}
-	
-	@GetMapping("/showPilots")
-	public String showPilots(Model model) {
-		showScuderias(model);
-		
+
+	public Integer showScuderias(Model model) {
+		String method = "GET";
+		StringBuilder requestPartTwo = new StringBuilder("http://localhost:8082/scuderias/showScuderias");
+
+		String responsePartTwo = HttpConnectionsAndRequests.doARequest(requestPartTwo.toString(), method);
+
+		Gson gson = new Gson();
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Scuderia> scuderias = gson.fromJson(responsePartTwo, ArrayList.class);
+		model.addAttribute("scuderias", scuderias);
+
+		return 202;
+	}
+
+	public Integer chargePilots(Model model) {
 		StringBuilder request = new StringBuilder("http://localhost:8082/pilots/chargePilots");
 		String method = "GET";
-		
+
 		String response = HttpConnectionsAndRequests.doARequest(request.toString(), method);
-		
-		if(response.equals("Cargados con exito")) {
-			StringBuilder requestPartTwo = new StringBuilder("http://localhost:8082/pilots/showPilots");
-			
-			String responsePartTwo = HttpConnectionsAndRequests.doARequest(requestPartTwo.toString(), method);
-			
-			Gson gson = new Gson();
-			
-			@SuppressWarnings("unchecked")
-			ArrayList<Pilot> pilots = gson.fromJson(responsePartTwo, ArrayList.class);
-			
-			System.out.println(pilots.toString());
-			
-			model.addAttribute("pilots", pilots);
-			
-			return "202";			
-		}else {
-			return "500";
+
+		if (response.equals("Cargados con exito")) {
+			return 202;
 		}
+
+		return 500;
+	}
+
+	public Integer showPilots(Model model) {
+		StringBuilder requestPartTwo = new StringBuilder("http://localhost:8082/pilots/showPilots");
+		String method = "GET";
+		
+		String responsePartTwo = HttpConnectionsAndRequests.doARequest(requestPartTwo.toString(), method);
+
+		Gson gson = new Gson();
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Pilot> pilots = gson.fromJson(responsePartTwo, ArrayList.class);
+
+		model.addAttribute("pilots", pilots);
+
+		return 202;
+	}
+
+	public Integer chargeCircuits(Model model) {
+		StringBuilder request = new StringBuilder("http://localhost:8082/circuits/chargeCircuits");
+		String method = "GET";
+
+		String response = HttpConnectionsAndRequests.doARequest(request.toString(), method);
+
+		if (response.equals("Cargados exitosamente")) {
+			return 202;
+		}
+
+		return 500;
 	}
 	
+	public Integer showCircuits(Model model) {
+		StringBuilder requestPartTwo = new StringBuilder("http://localhost:8082/circuits/showCircuits");
+		String method = "GET";
+		
+		String responsePartTwo = HttpConnectionsAndRequests.doARequest(requestPartTwo.toString(), method);
+
+		Gson gson = new Gson();
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Circuit> circuits = gson.fromJson(responsePartTwo, ArrayList.class);
+
+		model.addAttribute("circuits", circuits);
+
+		return 202;
+	}
 	
-//	public String showPilots() {
-//		
-//	}
+	public Integer chargeRaces(Model model) {
+		StringBuilder request = new StringBuilder("http://localhost:8082/races/chargeRaces");
+		String method = "GET";
+
+		String response = HttpConnectionsAndRequests.doARequest(request.toString(), method);
+
+		if (response.equals("Cargados con exito")) {
+			return 202;
+		}
+
+		return 500;
+	}
 	
+	public Integer showRaces(Model model) {
+		StringBuilder requestPartTwo = new StringBuilder("http://localhost:8082/races/showRaces");
+		String method = "GET";
+		
+		String responsePartTwo = HttpConnectionsAndRequests.doARequest(requestPartTwo.toString(), method);
+
+		Gson gson = new Gson();
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Race> races = gson.fromJson(responsePartTwo, ArrayList.class);
+
+		model.addAttribute("races", races);
+
+		return 202;
+	}
+	
+	@GetMapping("/chargeTheWiki")
+	public void chargeWiki(Model model) {
+		chargeScuderias(model);
+		chargePilots(model);
+		chargeCircuits(model);
+		chargeRaces(model);
+		
+		showScuderias(model);
+		showPilots(model);
+		showCircuits(model);
+		showRaces(model);
+	}
+
 }
